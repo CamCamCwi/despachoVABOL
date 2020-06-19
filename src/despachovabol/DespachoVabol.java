9254410bca494ec8573174337a4d9bc3dba10cb8
CamCamCwi <camilayacobc@gmail.com>
2020-06-19 15:01:48 -0400

    Cambios en el main del proyecto.

--- src/despachovabol/DespachoVabol.java
+++ src/despachovabol/DespachoVabol.java
@@ -1,13 +1,11 @@
 
 package despachovabol;
 
-import Negocio.NAnuncio;
 
 public class DespachoVabol {
 
     public static void main(String[] args) {
-        NAnuncio a = new NAnuncio();
-        a.holi();
+
     }
     
 }
