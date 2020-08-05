766323d4d62706219946883f2a3e08df557ef9da
Daniel <danielrobles1190@gmail.com>
2020-08-04 22:17:43 -0400

    Modifiqué algo

--- src/despachovabol/DespachoVabol.java
+++ src/despachovabol/DespachoVabol.java
@@ -12,9 +12,9 @@ public class DespachoVabol {
 
     public static void main(String[] args) {
 
-    Mmail mail = new Mmail();
-    mail.getMail(); 
     /*Mmail mail = new Mmail();
+    mail.getMail(); */
+    Mmail mail = new Mmail();
         
         int cantMails = mail.getCantidadMails();
         
@@ -29,7 +29,7 @@ public class DespachoVabol {
             } catch (InterruptedException ex) {
                 Logger.getLogger(DespachoVabol.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }*/
+        }
     }
     
 }
